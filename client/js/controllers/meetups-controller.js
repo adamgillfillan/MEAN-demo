86cfea1ad0a4dad09ed7c277eb8d66baa94c7027
Adam Gillfillan <adamgillfillan@vikings.berry.edu>
2015-02-06 18:55:04 -0500

    initial commit

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,17 +1,17 @@
 app.controller('meetupsController', ['$scope', '$resource', function($scope, $resource){
-	var Meetup = $resource('/RESTapi/meetups');
+    var Meetup = $resource('/RESTapi/meetups');
 
-	Meetup.query(function (results) {
-		$scope.meetups = results;
-	});
+    Meetup.query(function (results) {
+        $scope.meetups = results;
+    });
 
-	$scope.createMeetup = function(){
-		var meetup = new Meetup();
-		meetup.name = $scope.meetupName;
-		meetup.$save(function (result){
-			$scope.meetups.push(result);
-		});
-		$scope.meetupName = '';
-	};
+    $scope.createMeetup = function(){
+        var meetup = new Meetup();
+        meetup.name = $scope.meetupName;
+        meetup.$save(function (result){
+            $scope.meetups.push(result);
+        });
+        $scope.meetupName = '';
+    };
 
 }]);
